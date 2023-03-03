fd669274afb70cc67f643959430085d8cc422d4f
Diberia <2982910724@qq.com>
2023-03-03 10:00:47 +0800

    Spring 基于注解管理bean之注解和扫描

---
+++ spring_ioc_annotation/src/main/java/com/atguigu/spring/dao/impl/UserDaoImpl.java
@@ -0,0 +1,8 @@
+package com.atguigu.spring.dao.impl;
+
+import com.atguigu.spring.dao.UserDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+}
---
+++ spring_ioc_annotation/src/main/java/com/atguigu/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.atguigu.spring.service.impl;
+
+import com.atguigu.spring.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
