af0ec30d419d27d2b732bc5e9a311c1ff758d553
Diberia <2982910724@qq.com>
2023-03-04 19:44:09 +0800

    Spring 基于注解的自动装配之@Autowired的原理

--- spring_ioc_annotation/src/main/java/com/atguigu/spring/controller/UserController.java
+++ spring_ioc_annotation/src/main/java/com/atguigu/spring/controller/UserController.java
@@ -2,18 +2,22 @@ package com.atguigu.spring.controller;
 
 import com.atguigu.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 //@Controller("controller")
 @Controller
 public class UserController {
-
-    private UserService userService;
     @Autowired
+    @Qualifier("userServiceImpl")
+    private UserService userService;
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    public UserController() {
+    }
 
     /*public void setUserService(UserService userService) {
         this.userService = userService;
