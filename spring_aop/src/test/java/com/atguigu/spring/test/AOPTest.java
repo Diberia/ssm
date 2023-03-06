cc4545b463317d1f5a2c48254e6a2cb3a4824a4e
Diberia <2982910724@qq.com>
2023-03-06 20:58:00 +0800

    Spring 基于注解的AOP之环绕通知

--- spring_aop/src/test/java/com/atguigu/spring/test/AOPTest.java
+++ spring_aop/src/test/java/com/atguigu/spring/test/AOPTest.java
@@ -10,6 +10,6 @@ public class AOPTest {
     public void testAOPByAnnotation(){
         ApplicationContext ioc = new ClassPathXmlApplicationContext("aop-annotation.xml");
         Calculator calculator = ioc.getBean(Calculator.class);
-        calculator.div(1,0);
+        calculator.div(1,1);
     }
 }
