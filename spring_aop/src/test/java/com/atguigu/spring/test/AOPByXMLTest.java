cf878fc7bbf60cb68fe4033e5cba3a042d6ac614
Diberia <2982910724@qq.com>
2023-03-07 11:01:43 +0800

    Spring 基于xml的AOP实现

--- spring_aop/src/test/java/com/atguigu/spring/test/AOPByXMLTest.java
+++ spring_aop/src/test/java/com/atguigu/spring/test/AOPByXMLTest.java
@@ -1,15 +1,15 @@
 package com.atguigu.spring.test;
 
-import com.atguigu.spring.aop.annotation.Calculator;
+import com.atguigu.spring.aop.xml.Calculator;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AOPTest {
+public class AOPByXMLTest {
     @Test
-    public void testAOPByAnnotation(){
-        ApplicationContext ioc = new ClassPathXmlApplicationContext("aop-annotation.xml");
+    public void testAOPByXMl(){
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("aop-xml.xml");
         Calculator calculator = ioc.getBean(Calculator.class);
-        calculator.div(1,1);
+        calculator.add(1,1);
     }
 }
