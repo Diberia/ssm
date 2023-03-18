c546ccf306f61ec1770e194aa2e90b8e77b573eb
Diberia <2982910724@qq.com>
2023-03-18 10:03:54 +0800

    SpringMVC 使用Model、ModelMap、Map向请求域共享数据

--- spring_mvc_demo/src/main/java/com/atguigu/controller/TestScopeController.java
+++ spring_mvc_demo/src/main/java/com/atguigu/controller/TestScopeController.java
@@ -1,14 +1,24 @@
 package com.atguigu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 /**
  * 向域对象共享数据：
- * 1、通过ModelAndView向请求域共享数据
+ * 1.通过ModelAndView向请求域共享数据
  * 使用ModelAndView时，可以使用其Model功能向请求域共享数据
  * 使用View功能设置逻辑视图，但是控制器方法一定要将ModelANdView作为方法的返回值
+ * 2.使用Model向请求域共享数据
+ * 3.使用ModelMap向请求域共享数据
+ * 4.使用map向请求域共享数据
+ * 5.Model和ModelMap和Map的关系
+ * 其实在底层中，这些类型的形参最终都是通过org.springframework.validation.support.BindingAwareModelMap创建
+ * 四选一就行
  */
 @Controller
 public class TestScopeController {
@@ -25,5 +35,29 @@ public class TestScopeController {
         //设置逻辑视图
         modelAndView.setViewName("success");
         return modelAndView;
+    }
+
+    @RequestMapping("/test/model")
+    public String testModel(Model model){
+        //org.springframework.validation.support.BindingAwareModelMap
+        System.out.println(model.getClass().getName());
+        model.addAttribute("testRequestScope","hello,Model");
+        return "success";
+    }
+
+    @RequestMapping("/test/modelMap")
+    public String testModelMap(ModelMap modelMap){
+        //org.springframework.validation.support.BindingAwareModelMap
+        System.out.println(modelMap.getClass().getName());
+        modelMap.addAttribute("testRequestScope","hello,ModelMap");
+        return "success";
+    }
+
+    @RequestMapping("/test/map")
+    public String testMap(Map<String,Object> map){
+        //org.springframework.validation.support.BindingAwareModelMap
+        System.out.println(map.getClass().getName());
+        map.put("testRequestScope","hello,map");
+        return "success";
     }
 }
